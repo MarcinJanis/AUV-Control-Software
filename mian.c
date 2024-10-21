d357942bcd8784920b398622ae5bc543db5709d1
MarcinJanis <164472419+MarcinJanis@users.noreply.github.com>
2024-10-21 20:30:02 +0200

    Update mian.c

--- mian.c
+++ mian.c
@@ -25,6 +25,8 @@
 #include "AuvControl.h"
 #include <string.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,10 +42,17 @@
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
+int __io_putchar(int ch){
+	ITM_SendChar(ch);
+	return ch;
+}
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c3;
+
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
 DMA_HandleTypeDef hdma_usart2_tx;
@@ -51,7 +60,10 @@ DMA_HandleTypeDef hdma_usart2_tx;
 osThreadId defaultTaskHandle;
 osThreadId ReadInputHandle;
 osThreadId ProcInputDataHandle;
+osThreadId MapOutputHandle;
 osMutexId uartMutexHandle;
+osSemaphoreId inputsReaded_S_Handle;
+osSemaphoreId inputsCalculated_S_Handle;
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -62,9 +74,12 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C3_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM3_Init(void);
 void StartDefaultTask(void const * argument);
 void readInputFcn(void const * argument);
 void processInputDataFcn(void const * argument);
+void MapOutputFunction(void const * argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -75,19 +90,28 @@ void processInputDataFcn(void const * argument);
 
 /* Global Variables */
 #define Ts 0.1 // sampling time [s]
+#define TaskSamplingTime_ms 100
+// Raw data from IMU
 int16_t accellRaw[3];
-float accellScaled[3];
-float accellAngle[3];
 int16_t gyroRaw[3];
+// Scaled data from IMU
+float accellScaled[3]; // Accelerations in Local Frame
 float gyroScaled[3];
-float KF_errorMatrix[4];
+float gyroBias[3];
+// Angle calculated based on accelerometr
+float accellAngle[3];
+// Determined Orientation
 float orientationAngles[3];
 
+// diagnostics variables
+bool initRequest,NavigInitDone=false;
+HAL_StatusTypeDef mpuInitStatus,mpuCommStatus,uartStatus;
+TickType_t cycleStart,cycleDuration;
 // debugging variables:
-HAL_StatusTypeDef mpuInitStatus,mpuCommStatus;
-int temp1=0;
-float angleX,angleY,angleZ;
 
+int temp1=0;
+float gyroScaledFilterdZ[1];
+bool outputMapFcn;
 
 /* USER CODE END 0 */
 
@@ -123,10 +147,12 @@ int main(void)
   MX_DMA_Init();
   MX_USART2_UART_Init();
   MX_I2C3_Init();
+  MX_TIM1_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
   mpuInitStatus = mpu6050_init(&hi2c3,MPU6050_ADDRESS);
-
+  initRequest = true;
   /* USER CODE END 2 */
 
   /* Create the mutex(es) */
@@ -137,6 +163,15 @@ int main(void)
   /* USER CODE BEGIN RTOS_MUTEX */
   /* add mutexes, ... */
   /* USER CODE END RTOS_MUTEX */
+
+  /* Create the semaphores(s) */
+  /* definition and creation of inputsReaded_S_ */
+  osSemaphoreDef(inputsReaded_S_);
+  inputsReaded_S_Handle = osSemaphoreCreate(osSemaphore(inputsReaded_S_), 1);
+
+  /* definition and creation of inputsCalculated_S_ */
+  osSemaphoreDef(inputsCalculated_S_);
+  inputsCalculated_S_Handle = osSemaphoreCreate(osSemaphore(inputsCalculated_S_), 1);
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
   /* add semaphores, ... */
@@ -156,12 +191,16 @@ int main(void)
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* definition and creation of ReadInput */
-  osThreadDef(ReadInput, readInputFcn, osPriorityIdle, 0, 128);
+  osThreadDef(ReadInput, readInputFcn, osPriorityRealtime, 0, 128);
   ReadInputHandle = osThreadCreate(osThread(ReadInput), NULL);
 
   /* definition and creation of ProcInputData */
-  osThreadDef(ProcInputData, processInputDataFcn, osPriorityIdle, 0, 128);
+  osThreadDef(ProcInputData, processInputDataFcn, osPriorityHigh, 0, 256);
   ProcInputDataHandle = osThreadCreate(osThread(ProcInputData), NULL);
+
+  /* definition and creation of MapOutput */
+  osThreadDef(MapOutput, MapOutputFunction, osPriorityNormal, 0, 256);
+  MapOutputHandle = osThreadCreate(osThread(MapOutput), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -264,6 +303,144 @@ static void MX_I2C3_Init(void)
 }
 
 /**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 65535;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -326,9 +503,10 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_BOARD_GPIO_Port, LED_BOARD_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_BOARD_Pin|SPI_SS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : BUTTON_BOARD_Pin */
   GPIO_InitStruct.Pin = BUTTON_BOARD_Pin;
@@ -336,12 +514,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(BUTTON_BOARD_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LED_BOARD_Pin */
-  GPIO_InitStruct.Pin = LED_BOARD_Pin;
+  /*Configure GPIO pins : LED_BOARD_Pin SPI_SS_Pin */
+  GPIO_InitStruct.Pin = LED_BOARD_Pin|SPI_SS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_BOARD_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
@@ -361,11 +539,42 @@ static void MX_GPIO_Init(void)
 void StartDefaultTask(void const * argument)
 {
   /* USER CODE BEGIN 5 */
+	float gyroBiasSum[3]={0,0,0};
+	const TickType_t initTime = pdMS_TO_TICKS(3000); // 3 seconds
+	TickType_t pressStartTime = 0;
+	int n=0;
+
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+	  if (initRequest == true){
+	  	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	  		if (pressStartTime == 0) {
+	  			      // Start the timer
+	  			  pressStartTime = xTaskGetTickCount();
+	  			  }
+	  		else if((xTaskGetTickCount() - pressStartTime) < initTime){
+	  				  	 for (int i=0;i<=2;i++){
+	  				  		 gyroBiasSum[i]+=gyroScaled[i];
+
+	  				  	 }
+	  				  	n++;
+	  				  	 vTaskDelay(100);
+	  			  }
+	  		else{
+
+	  			  for (int i=1;i<=2;i++){
+	  				  gyroBias[i]=gyroBiasSum[i]/n;
+	  			  	  }
+	  			  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	  			  pressStartTime = 0;
+	  			  n=0;
+	  			  NavigInitDone = true;
+	  			  initRequest = false;
+	  			 }
+	  	}
   }
+
   /* USER CODE END 5 */
 }
 
@@ -382,9 +591,12 @@ void readInputFcn(void const * argument)
   /* Infinite loop */
   for(;;)
   {
+	  cycleStart =xTaskGetTickCount();
+	  //osSemaphoreId inputsReaded_S_Handle;
+	  //osSemaphoreId inputsCaluclated_S_Handle;
 	mpuCommStatus = mpu6050_update(&hi2c3,MPU6050_ADDRESS,accellRaw,gyroRaw);
-	temp1++;
-	vTaskDelay(pdMS_TO_TICKS(1000));
+	xSemaphoreGive(inputsReaded_S_Handle);
+	vTaskDelay(pdMS_TO_TICKS(TaskSamplingTime_ms));
   }
   /* USER CODE END readInputFcn */
 }
@@ -402,17 +614,51 @@ void processInputDataFcn(void const * argument)
   /* Infinite loop */
   for(;;)
   {
-
-
+	  if(xSemaphoreTake(inputsReaded_S_Handle,portMAX_DELAY)==pdTRUE){
+	  //outputMapFcn = true;
 	  accelCalc(accellRaw, accellScaled , accellAngle);
-	  angleX = accellAngle[0];
-	  angleY = accellAngle[1];
-	  angleZ = accellAngle[2];
-	  //char *message = "abcdefg";
-	  //HAL_UART_Transmit_DMA(&huart2, (uint8_t *)message, strlen(message));
-	  vTaskDelay(pdMS_TO_TICKS(1000));
+	  gyroCalc(gyroRaw,gyroScaled);
+	  kalmanFilter(accellAngle, gyroScaled, orientationAngles, NavigInitDone);
+	  //orientationAngles[2]+= Ts*ellipFilter(gyroScaled,gyroScaledFilterdZ);
+	  YawEvaluate(orientationAngles,gyroScaled,gyroBias[2],NavigInitDone);
+	  if (NavigInitDone == true)NavigInitDone=false;
+	  //outputMapFcn = false;
+	  xSemaphoreGive(inputsCalculated_S_Handle);
+	  //vTaskDelay(pdMS_TO_TICKS(TaskSamplingTime_ms));
+	  }
   }
   /* USER CODE END processInputDataFcn */
+}
+
+/* USER CODE BEGIN Header_MapOutputFunction */
+/**
+* @brief Function implementing the MapOutput thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_MapOutputFunction */
+void MapOutputFunction(void const * argument)
+{
+  /* USER CODE BEGIN MapOutputFunction */
+  /* Infinite loop */
+  for(;;)
+  {
+	 if(xSemaphoreTake(inputsCalculated_S_Handle,portMAX_DELAY)==pdTRUE){
+	char buff[50];
+	printf("RawData, %d, %d, %d, %d, %d, %d\n" ,accellRaw[0],accellRaw[1],accellRaw[2],gyroRaw[0],gyroRaw[1],gyroRaw[2]);
+	sprintf(buff,"%.2f, %.2f, %.2f\n",orientationAngles[0],orientationAngles[1],orientationAngles[2]);
+	printf(buff);
+	//sprintf(buff,"Orientation, %.2f, %.2f, %.2f\n",orientationAngles[0],orientationAngles[1],orientationAngles[2]);
+	//printf(buff);
+
+	//printf("ScaledData, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n",accellScaled[0],accellScaled[1],accellScaled[2],gyroScaled[0],gyroScaled[1],gyroScaled[2]);
+	//HAL_GPIO_WritePin(GPIOA, SPI_SS_Pin , GPIO_PIN_RESET);
+	//uartStatus=HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 300);
+	//HAL_GPIO_WritePin(GPIOA, SPI_SS_Pin, GPIO_PIN_SET);
+		cycleDuration = (cycleStart  - xTaskGetTickCount()); // calculate in ms
+	 }
+  }
+  /* USER CODE END MapOutputFunction */
 }
 
 /**
