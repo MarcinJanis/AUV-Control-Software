88728e9d5b3b0af9fc0feecd70a2da578ed25a64
MarcinJanis <164472419+MarcinJanis@users.noreply.github.com>
2024-10-21 20:31:44 +0200

    Update AuvControl.h

--- AuvControl.h
+++ AuvControl.h
@@ -1,5 +1,6 @@
 #include "stm32f4xx_hal.h"
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifndef AuvControl_h
 #define AuvControl_h
@@ -7,5 +8,10 @@
 HAL_StatusTypeDef mpu6050_init(I2C_HandleTypeDef *hi2c, uint8_t ADDRESS);
 HAL_StatusTypeDef mpu6050_update(I2C_HandleTypeDef *hi2c, uint8_t ADDRESS,int16_t AccelDataRaw[3], int16_t GyroDataRaw[3]);
 void accelCalc(int16_t AccelDataRaw[3],float Acceleration[3],float AccelAngle[3]);
-
+void gyroCalc(int16_t GyroDataRaw[3], float GyroScaledData[3]);
+void kalmanFilter(float AccellAngle[3], float GyroScaledData[3], float OrientationAngles[3], bool NavigInitDone);
+float ellipFilter(float GyroScaled[3], float controlOutput[1], bool NavigInitDone);
+void YawEvaluate(float orientationAngles[3], float GyroScaled[3],float gyroBias,bool NavigInitDone);
+//void navigInit(float gyroScaled[3],float gyroBias[3]);
+void navigInitCheck();
 #endif
