d3992f2c43628debd12c34654ae24c60718499a6
MarcinJanis <164472419+MarcinJanis@users.noreply.github.com>
2024-10-25 08:36:18 +0200

    Update AuvControl.h

--- AuvControl.h
+++ AuvControl.h
@@ -17,9 +17,9 @@ HAL_StatusTypeDef mpu6050_init(I2C_HandleTypeDef *hi2c, uint8_t ADDRESS);
 HAL_StatusTypeDef mpu6050_update(I2C_HandleTypeDef *hi2c, uint8_t ADDRESS,int16_t AccelDataRaw[3], int16_t GyroDataRaw[3]);
 void accelCalc(int16_t AccelDataRaw[3],float Acceleration[3],float AccelAngle[3]);
 void gyroCalc(int16_t GyroDataRaw[3], float GyroScaledData[3]);
-void kalmanFilter(float AccellAngle[3], float GyroScaledData[3], float OrientationAngles[3],float AccellScaled[3],float gyroBias[3], bool NavigInitDone);
+void kalmanFilter(float AccellAngle[3], float GyroGlobal[3], float OrientationAngles[3], float AccellScaled[3],float gyroBias[3], bool NavigInitDone);
 float ellipFilter(float GyroScaled[3], float controlOutput[1], bool NavigInitDone);
-void YawEvaluate(float orientationAngles[3], float GyroScaled[3],float gyroBias[3],bool NavigInitDone,float debugMatrix[2]);
+void YawEvaluate(float orientationAngles[3], float GyroScaled[3],float gyroBias[3],bool NavigInitDone,float gyroGlobal[3]);
 //void navigInit(float gyroScaled[3],float gyroBias[3]);
 void navigInitCheck();
 #endif
